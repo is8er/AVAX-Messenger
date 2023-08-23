36be96f9e936875b90d81a7b2f99bef1ce80d3df
tai_shimoda <tai_shimoda@mcinc.jp>
2023-08-23 16:43:42 +0900

    Section 1 - Lesson 3

--- packages/contract/contracts/Messenger.sol
+++ packages/contract/contracts/Messenger.sol
@@ -6,11 +6,49 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract Messenger {
-    uint256 public state;
+    // メッセージ情報を定義します。
+    struct Message {
+        address payable sender;
+        address payable receiver;
+        uint256 depositInWei;
+        uint256 timestamp;
+        string text;
+        bool isPending;
+    }
 
-    constructor() {
+    // メッセージの受取人アドレスをkeyにメッセージを保存します。
+    mapping(address => Message[]) private _messagesAtAddress;
+
+    constructor() payable {
         console.log("Here is my first smart contract!");
+    }
+    
+    // ユーザからメッセージを受け取り、状態変数に格納します。
+    function post(string memory _text, address payable _receiver)
+        public
+        payable
+    {
+        console.log(
+            "%s posts text:[%s] token:[%d]",
+            msg.sender,
+            _text,
+            msg.value
+        );
+    
+        _messagesAtAddress[_receiver].push(
+            Message(
+                payable(msg.sender),
+                _receiver,
+                msg.value,
+                block.timestamp,
+                _text,
+                true
+            )
+        );
+    }
 
-        state = 1;
+    // ユーザのアドレス宛のメッセージを全て取得します。
+    function getOwnMessages() public view returns (Message[] memory) {
+        return _messagesAtAddress[msg.sender];
     }
 }
--- packages/contract/test/Messenger.ts
+++ packages/contract/test/Messenger.ts
@@ -1,11 +1,58 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import hre from 'hardhat';
+import { Overrides } from 'ethers';
+import hre, { ethers } from 'hardhat';
 
 describe('Messenger', function () {
-  it('construct', async function () {
-    const Messenger = await hre.ethers.getContractFactory('Messenger');
-    const messenger = await Messenger.deploy();
+  async function deployContract() {
+    // 初めのアドレスはコントラクトのデプロイに使用されます。
+    const [owner, otherAccount] = await ethers.getSigners();
 
-    expect(await messenger.state()).to.equal(1);
+    const funds = 100;
+
+    const Messenger = await hre.ethers.getContractFactory('Messenger');
+    const messenger = await Messenger.deploy({
+      value: funds,
+    } as Overrides);
+
+    return { messenger, funds, owner, otherAccount };
+  }
+
+  describe('Post', function () {
+    it('Should send the correct amount of tokens', async function () {
+      const { messenger, owner, otherAccount } = await loadFixture(
+        deployContract
+      );
+      const test_deposit = 10;
+
+      // メッセージをpostした場合は、送り主(owner)からコントラクト(messenger)へ送金されます。
+      await expect(
+        messenger.post('text', otherAccount.address, {
+          value: test_deposit,
+        })
+      ).to.changeEtherBalances(
+        [owner, messenger],
+        [-test_deposit, test_deposit]
+      );
+    });
+
+    it('Should set the right Message', async function () {
+      const { messenger, owner, otherAccount } = await loadFixture(
+        deployContract
+      );
+      const test_deposit = 1;
+      const test_text = 'text';
+
+      await messenger.post(test_text, otherAccount.address, {
+        value: test_deposit,
+      });
+      const messages = await messenger.connect(otherAccount).getOwnMessages();
+      const message = messages[0];
+      expect(message.depositInWei).to.equal(test_deposit);
+      expect(message.text).to.equal(test_text);
+      expect(message.isPending).to.equal(true);
+      expect(message.sender).to.equal(owner.address);
+      expect(message.receiver).to.equal(otherAccount.address);
+    });
   });
 });
