86db79f1650f0f5f59450c6a07bc6fcffe4521ff
tai_shimoda <tai_shimoda@mcinc.jp>
2023-08-23 16:56:34 +0900

    Section 1 - Lesson 4

--- packages/contract/contracts/Messenger.sol
+++ packages/contract/contracts/Messenger.sol
@@ -47,6 +47,51 @@ contract Messenger {
         );
     }
 
+    // メッセージ受け取りを承諾して、AVAXを受け取ります。
+    function accept(uint256 _index) public {
+        //指定インデックスのメッセージを確認します。
+        _confirmMessage(_index);
+
+        Message memory message = _messagesAtAddress[msg.sender][_index];
+
+        // メッセージの受取人にavaxを送信します。
+        _sendAvax(message.receiver, message.depositInWei);
+    }
+
+    // メッセージ受け取りを却下して、AVAXをメッセージ送信者へ返却します。
+    function deny(uint256 _index) public payable {
+        _confirmMessage(_index);
+
+        Message memory message = _messagesAtAddress[msg.sender][_index];
+
+        // メッセージの送信者にavaxを返却します。
+        _sendAvax(message.sender, message.depositInWei);
+    }
+
+    function _confirmMessage(uint256 _index) private {
+        Message storage message = _messagesAtAddress[msg.sender][_index];
+
+        // 関数を呼び出したアドレスとメッセージの受取人アドレスが同じか確認します。
+        require(
+            msg.sender == message.receiver,
+            "Only the receiver can _confirmMessage the message"
+        );
+
+        // メッセージが保留中であることを確認します。
+        require(
+            message.isPending == true,
+            "This message has already been confirmed"
+        );
+
+        // メッセージの保留状態を解除します。
+        message.isPending = false;
+    }
+
+    function _sendAvax(address payable _to, uint256 _amountInWei) private {
+        (bool success, ) = (_to).call{value: _amountInWei}("");
+        require(success, "Failed to withdraw AVAX from contract");
+    }
+
     // ユーザのアドレス宛のメッセージを全て取得します。
     function getOwnMessages() public view returns (Message[] memory) {
         return _messagesAtAddress[msg.sender];
--- packages/contract/test/Messenger.ts
+++ packages/contract/test/Messenger.ts
@@ -1,47 +1,37 @@
-import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
-import { expect } from 'chai';
-import { Overrides } from 'ethers';
-import hre, { ethers } from 'hardhat';
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { Overrides } from "ethers";
+import hre, { ethers } from "hardhat";
 
-describe('Messenger', function () {
+describe("Messenger", function () {
   async function deployContract() {
     // 初めのアドレスはコントラクトのデプロイに使用されます。
     const [owner, otherAccount] = await ethers.getSigners();
 
     const funds = 100;
 
-    const Messenger = await hre.ethers.getContractFactory('Messenger');
-    const messenger = await Messenger.deploy({
-      value: funds,
-    } as Overrides);
-
+    const Messenger = await hre.ethers.getContractFactory("Messenger");
+    const messenger = await Messenger.deploy({ value: funds });
     return { messenger, funds, owner, otherAccount };
   }
 
-  describe('Post', function () {
-    it('Should send the correct amount of tokens', async function () {
-      const { messenger, owner, otherAccount } = await loadFixture(
-        deployContract
-      );
+  describe("Post", function () {
+    it("Should send the correct amount of tokens", async function () {
+      const { messenger, owner, otherAccount } = await loadFixture(deployContract);
       const test_deposit = 10;
 
       // メッセージをpostした場合は、送り主(owner)からコントラクト(messenger)へ送金されます。
       await expect(
-        messenger.post('text', otherAccount.address, {
+        messenger.post("text", otherAccount.address, {
           value: test_deposit,
         })
-      ).to.changeEtherBalances(
-        [owner, messenger],
-        [-test_deposit, test_deposit]
-      );
+      ).to.changeEtherBalances([owner, messenger], [-test_deposit, test_deposit]);
     });
 
-    it('Should set the right Message', async function () {
-      const { messenger, owner, otherAccount } = await loadFixture(
-        deployContract
-      );
+    it("Should set the right Message", async function () {
+      const { messenger, owner, otherAccount } = await loadFixture(deployContract);
       const test_deposit = 1;
-      const test_text = 'text';
+      const test_text = "text";
 
       await messenger.post(test_text, otherAccount.address, {
         value: test_deposit,
@@ -53,6 +43,88 @@ describe('Messenger', function () {
       expect(message.isPending).to.equal(true);
       expect(message.sender).to.equal(owner.address);
       expect(message.receiver).to.equal(otherAccount.address);
+    });
+  });
+
+  describe("Accept", function () {
+    it("isPending must be changed", async function () {
+      const { messenger, otherAccount } = await loadFixture(deployContract);
+      const first_index = 0;
+
+      await messenger.post("text", otherAccount.address);
+      let messages = await messenger.connect(otherAccount).getOwnMessages();
+      expect(messages[0].isPending).to.equal(true);
+
+      await messenger.connect(otherAccount).accept(first_index);
+      messages = await messenger.connect(otherAccount).getOwnMessages();
+      expect(messages[0].isPending).to.equal(false);
+    });
+
+    it("Should send the correct amount of tokens", async function () {
+      const { messenger, otherAccount } = await loadFixture(deployContract);
+      const test_deposit = 10;
+
+      await messenger.post("text", otherAccount.address, {
+        value: test_deposit,
+      });
+
+      // メッセージをacceptした場合は、コントラクト(messenger)から受取人(otherAccount)へ送金されます。
+      const first_index = 0;
+      await expect(messenger.connect(otherAccount).accept(first_index)).to.changeEtherBalances(
+        [messenger, otherAccount],
+        [-test_deposit, test_deposit]
+      );
+    });
+
+    it("Should revert with the right error if called in duplicate", async function () {
+      const { messenger, otherAccount } = await loadFixture(deployContract);
+
+      await messenger.post("text", otherAccount.address, { value: 1 });
+      await messenger.connect(otherAccount).accept(0);
+      await expect(messenger.connect(otherAccount).accept(0)).to.be.revertedWith(
+        "This message has already been confirmed"
+      );
+    });
+  });
+
+  describe("Deny", function () {
+    it("isPending must be changed", async function () {
+      const { messenger, otherAccount } = await loadFixture(deployContract);
+      const first_index = 0;
+
+      await messenger.post("text", otherAccount.address);
+      let messages = await messenger.connect(otherAccount).getOwnMessages();
+      expect(messages[0].isPending).to.equal(true);
+
+      await messenger.connect(otherAccount).deny(first_index);
+      messages = await messenger.connect(otherAccount).getOwnMessages();
+      expect(messages[0].isPending).to.equal(false);
+    });
+
+    it("Should send the correct amount of tokens", async function () {
+      const { messenger, owner, otherAccount } = await loadFixture(deployContract);
+      const test_deposit = 10;
+
+      await messenger.post("text", otherAccount.address, {
+        value: test_deposit,
+      });
+
+      // メッセージをdenyした場合は、コントラクト(messenger)から送信者(owner)へ送金されます。
+      const first_index = 0;
+      await expect(messenger.connect(otherAccount).deny(first_index)).to.changeEtherBalances(
+        [messenger, owner],
+        [-test_deposit, test_deposit]
+      );
+    });
+
+    it("Should revert with the right error if called in duplicate", async function () {
+      const { messenger, otherAccount } = await loadFixture(deployContract);
+
+      await messenger.post("text", otherAccount.address, { value: 1 });
+      await messenger.connect(otherAccount).deny(0);
+      await expect(messenger.connect(otherAccount).deny(0)).to.be.revertedWith(
+        "This message has already been confirmed"
+      );
     });
   });
 });
